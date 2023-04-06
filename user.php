8a8151e7b4f3c5a3eb2187a97adfd75da3a5d0d4
KaranThakur-77 <thakurkaran.in@gmail.com>
2023-04-06 09:54:30 +0545

    Added drop-shadow using CSS.

--- user.php
+++ user.php
@@ -5,15 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="user.css">
+    <link rel="stylesheet" href="user1.css">
 </head>
 <body>
     <div class="header">
         <h1>Hello User.</h1>
     </div>
-    <div class="container">
+    <div class="box">
+    <div class="content">
         <h1>Invalid Credentials.</h1>
         <button class="btn"><a href="index.php">Try again.</a></button>
+    </div>
+    <div class="imgBox">
+        <img src="img1.png" alt=" an image" class="image">
+    </div>
     </div>
 </body>
 </html>
